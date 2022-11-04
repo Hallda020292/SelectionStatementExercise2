a9fb7d775146012b18aa1803086e784ab3551cc0
Dakota hall <Hallda020292@gmail.com>
2022-11-04 11:26:41 -0400

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Linq.Expressions;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("what is your favorite school subject?");
+            var reponse = Console.ReadLine();
+
+            switch (reponse)
+            {
+                case "math":
+                    Console.WriteLine($"math is fun!");
+                    break;
+                case "English":
+                    Console.WriteLine("thats cool but im illiterate");
+                    break;
+                default:
+                    Console.WriteLine("Never heard if that one, must be new!");
+                    break;
+
+
+
+            }
+
+
+
+
+       
         }
+
     }
+
 }
